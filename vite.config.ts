990ae037f1ce58b70fc0a14a2951a0c4e2a1f9c8
xuebin80 <576254@qq.com>
2026-06-10 13:49:34 +0800

    修复vite base默认值'/'避免本地dev logo不显示

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE || '/yanglingsanzhi/',
+  base: process.env.VITE_BASE || '/',
   plugins: [react()],
   resolve: {
     alias: {
